0eacb4a4921da3d8b59b16ffe58849f78fc26c7b
Christian Bomprezzi <chrsz@Axl.homenet.telecomitalia.it>
2024-05-27 14:45:46 +0200

    modify requirements

--- requirements.py
+++ requirements.py
@@ -2,5 +2,9 @@ asgiref==3.8.1
 Django==4.2.13
 django-rest-framework==0.1.0
 djangorestframework==3.15.1
+Jinja2==3.1.4
+MarkupSafe==2.1.5
+pdfkit==1.0.0
+pillow==10.3.0
 sqlparse==0.5.0
 typing_extensions==4.12.0
